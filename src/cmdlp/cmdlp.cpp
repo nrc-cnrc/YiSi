53fdc7b55cb8f97692383bffe82b50474ae583a1
Darlene Stewart <Darlene.Stewart@nrc-cnrc.gc.ca>
2018-10-22 14:40:49 -0400

    [cmdlp] Undid some local changes from the initial commit of Jackie's YiSi code.

--- src/cmdlp/cmdlp.cpp
+++ src/cmdlp/cmdlp.cpp
@@ -75,7 +75,7 @@ void com::masaers::cmdlp::value_option<com::masaers::cmdlp::config_files>::evalu
 
 com::masaers::cmdlp::parser::~parser() {
   using namespace std;
-  for (auto it = options_m.begin(); it != options_m.end(); ++it) {
+  for (auto it = begin(options_m); it != end(options_m); ++it) {
     delete *it;
     *it = NULL;
   }
@@ -140,7 +140,7 @@ bool com::masaers::cmdlp::parser::bind(option_i* opt, const char flag) {
     std::ostringstream s;
     s << "Failed to bind option to flag '-" << flag << "'. "
       << "It already exists.";
-    exit(1);
+    throw std::runtime_error(s.str().c_str());
   } else {
     bindings_m[opt].second.push_back(flag);
   }
@@ -153,7 +153,7 @@ bool com::masaers::cmdlp::parser::bind(option_i* opt, const std::string& name) {
     std::ostringstream s;
     s << "Failed to bind option to name '--" << name << "'. "
       << "It already exists.";
-    exit(1);
+    throw std::runtime_error(s.str().c_str());
   } else {
     bindings_m[opt].first.push_back(name);
   }
@@ -162,20 +162,20 @@ bool com::masaers::cmdlp::parser::bind(option_i* opt, const std::string& name) {
 
 void com::masaers::cmdlp::parser::print_call(std::ostream& s, const std::vector<std::string>& names, std::vector<char> flags, bool print_all) {
   using namespace std;
-  for (auto it = names.begin(); it != names.end(); ++it) {
-    if (it != names.begin()) {
+  for (auto it = begin(names); it != end(names); ++it) {
+    if (it != begin(names)) {
       s << "|";
     }
-    const auto pos = find(flags.begin(), flags.end(), it->at(0));
-    if (pos != flags.end()) {
+    const auto pos = find(begin(flags), end(flags), it->front());
+    if (pos != end(flags)) {
       flags.erase(pos);
-      s << "-[-" << it->at(0) << "]" << it->substr(1);
+      s << "-[-" << it->front() << "]" << it->substr(1);
     } else {
       s << "--" << *it;
     }
   }
-  for (auto it = flags.begin(); it != flags.end(); ++it) {
-    if (names.size() != 0 || it != flags.begin()) {
+  for (auto it = begin(flags); it != end(flags); ++it) {
+    if (names.size() != 0 || it != begin(flags)) {
       s << "|";
     }
     s << "-" << *it;

f0c73912b640b70e35e24d0ca0e9b90014ad8ab6
Darlene Stewart <Darlene.Stewart@nrc-cnrc.gc.ca>
2018-10-30 23:39:21 -0400

    Fixed formatting in Mate.java.

--- src/Mate.java
+++ src/Mate.java
@@ -1,12 +1,14 @@
 /**
- * @author Jackie Lo
  * @file Mate.java
+ * @brief Bridging class for MATE
+ * 
+ * @author Jackie Lo
  *
- *
- * COMMENTS:
- *
- * Bridging class for MATE
- *
+ * Multilingual Text Processing / Traitement multilingue de textes
+ * Digital Technologies Research Centre / Centre de recherche en technologies numériques
+ * National Research Council Canada / Conseil national de recherches Canada
+ * Copyright 2018, Her Majesty in Right of Canada /
+ * Copyright 2018, Sa Majeste la Reine du Chef du Canada
  */
 
 package yisi;
@@ -21,78 +23,78 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class Mate {
-    protected CompletePipeline pipeline =null;
-    
-    public String init(String lang,
-		       boolean rerank,
-		       boolean hybrid,
-		       String token,
-		       String morph,
-		       String lemma,
-		       String parser,
-		       String tagger,
-		       String srl) {
-	String result= new String();
-	try {
-	    System.setOut(System.err);
-	    ArrayList<String> argsBuilder = new ArrayList<String>();
-	    argsBuilder.add(lang);
-	    argsBuilder.add("-tokenize");
-	    if (rerank){
-		argsBuilder.add("-reranker");
-	    }
-	    if (hybrid) {
-		argsBuilder.add("-hybrid");
-	    }
-	    if (! token.isEmpty()){
-		argsBuilder.add("-token");
-		argsBuilder.add(token);
-	    }
-	    if (! morph.isEmpty()){
-                argsBuilder.add("-morph");
-                argsBuilder.add(morph);
-            }
-	    if (! lemma.isEmpty()){
-                argsBuilder.add("-lemma");
-                argsBuilder.add(lemma);
-            }
-	    if (! tagger.isEmpty()){
-                argsBuilder.add("-tagger");
-                argsBuilder.add(tagger);
-            }
-	    if (! parser.isEmpty()){
-                argsBuilder.add("-parser");
-                argsBuilder.add(parser);
-            }
-	    if (! srl.isEmpty()){
-                argsBuilder.add("-srl");
-                argsBuilder.add(srl);
-            }
-	    String[] args = new String[argsBuilder.size()];
-	    argsBuilder.toArray(args);
-	    System.err.println(java.util.Arrays.toString(args));
-	    CompletePipelineCMDLineOptions options = new CompletePipelineCMDLineOptions();
-	    options.parseCmdLineArgs(args);
-	    String error = FileExistenceVerifier.verifyCompletePipelineAllNecessaryModelFiles(options);
-	    if (error != null){
-		result += error + "\n";
-	    } else {
-		pipeline = CompletePipeline.getCompletePipeline(options);
-	    }
-	}catch (Exception e){
-	    result += e.getMessage();
-	}
-	return result;
-    }
-    
-    public String parse(String sentence){
-	String result = null;
-	try {
-	    result = pipeline.parse(sentence).toString();
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    System.err.println(sentence);
-	} 
-	return result;
-    }
+   protected CompletePipeline pipeline = null;
+
+   public String init(String lang,
+                      boolean rerank,
+                      boolean hybrid,
+                      String token,
+                      String morph,
+                      String lemma,
+                      String parser,
+                      String tagger,
+                      String srl) {
+      String result= new String();
+      try {
+         System.setOut(System.err);
+         ArrayList<String> argsBuilder = new ArrayList<String>();
+         argsBuilder.add(lang);
+         argsBuilder.add("-tokenize");
+         if (rerank){
+            argsBuilder.add("-reranker");
+         }
+         if (hybrid) {
+            argsBuilder.add("-hybrid");
+         }
+         if (! token.isEmpty()){
+            argsBuilder.add("-token");
+            argsBuilder.add(token);
+         }
+         if (! morph.isEmpty()){
+            argsBuilder.add("-morph");
+            argsBuilder.add(morph);
+         }
+         if (! lemma.isEmpty()){
+            argsBuilder.add("-lemma");
+            argsBuilder.add(lemma);
+         }
+         if (! tagger.isEmpty()){
+            argsBuilder.add("-tagger");
+            argsBuilder.add(tagger);
+         }
+         if (! parser.isEmpty()){
+            argsBuilder.add("-parser");
+            argsBuilder.add(parser);
+         }
+         if (! srl.isEmpty()){
+            argsBuilder.add("-srl");
+            argsBuilder.add(srl);
+         }
+         String[] args = new String[argsBuilder.size()];
+         argsBuilder.toArray(args);
+         System.err.println(java.util.Arrays.toString(args));
+         CompletePipelineCMDLineOptions options = new CompletePipelineCMDLineOptions();
+         options.parseCmdLineArgs(args);
+         String error = FileExistenceVerifier.verifyCompletePipelineAllNecessaryModelFiles(options);
+         if (error != null){
+            result += error + "\n";
+         } else {
+            pipeline = CompletePipeline.getCompletePipeline(options);
+         }
+      } catch (Exception e){
+         result += e.getMessage();
+      }
+      return result;
+   }
+
+   public String parse(String sentence) {
+      String result = null;
+      try {
+         result = pipeline.parse(sentence).toString();
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(sentence);
+      } 
+      return result;
+   }
 }

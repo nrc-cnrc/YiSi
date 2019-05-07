efa1ea1014ed88910e42572720cf626423bc1713
Jackie Lo <loc982@usr-svc-sci-ctn070.science.gc.ca>
2019-05-07 15:59:09 -0400

    Added some handy tools for general w2v embeddings analysis.
    (cherry picked from NRC-private branch commit 7bd1fca5447cfc176da26a48fdebb90f9d602da7)

---
+++ src/oov_test.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file w2v_test.cpp
+ * @brief Unit test for w2v lexsim.
+ *
+ * @author Jackie Lo
+ *
+ * Multilingual Text Processing / Traitement multilingue de textes
+ * Digital Technologies Research Centre / Centre de recherche en technologies numériques
+ * National Research Council Canada / Conseil national de recherches Canada
+ * Copyright 2018, Her Majesty in Right of Canada /
+ * Copyright 2018, Sa Majeste la Reine du Chef du Canada
+ */
+
+#include "lexsim.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace yisi;
+
+int main(int argc, char* argv[])
+{
+   lexsim_t w2vtxt("w2v", argv[1], "cosine");
+   string sent;
+   while(!cin.eof()){
+     getline(cin, sent);
+     //cout <<sent<<endl;
+     auto tokens = tokenize(sent);
+     for (auto it=tokens.begin(); it!=tokens.end(); it++){
+       if ((w2vtxt.get_wv(*it,HYP_MODE)).size() == 0){
+	 cout << *it << endl;
+       }
+     }
+   }
+   return 0;
+}
+

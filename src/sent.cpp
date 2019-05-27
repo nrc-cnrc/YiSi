d0022bcd5b53b5787f2ca4e8c628d345d4e2c8f7
Darlene Stewart <Darlene.Stewart@nrc-cnrc.gc.ca>
2019-05-27 14:27:02 -0400

    Fixed copyright year.

--- src/sent.cpp
+++ src/sent.cpp
@@ -11,8 +11,8 @@
  * Multilingual Text Processing / Traitement multilingue de textes
  * Digital Technologies Research Centre / Centre de recherche en technologies numériques
  * National Research Council Canada / Conseil national de recherches Canada
- * Copyright 2018, Her Majesty in Right of Canada /
- * Copyright 2018, Sa Majeste la Reine du Chef du Canada
+ * Copyright 2019, Her Majesty in Right of Canada /
+ * Copyright 2019, Sa Majeste la Reine du Chef du Canada
  */
 
 #include "sent.h"
